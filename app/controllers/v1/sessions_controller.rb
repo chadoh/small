7a8d851ee3f47fc92d18d73b1ccfd8be6aa20104
Chad Ostrowski <hi@chadoh.com>
2015-06-19 14:02:58 -0400

    Fix some bugs

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,10 +4,10 @@ module V1
 
     # POST /v1/login
     def create
-      @user = User.find_for_database_authentication(email: params[:username])
+      @user = User.find_for_database_authentication(email: user_params[:email])
       return invalid_login_attempt unless @user
 
-      if @user.valid_password?(params[:password])
+      if @user.valid_password?(user_params[:password])
         sign_in :user, @user
         render json: @user, serializer: SessionSerializer, root: nil
       else
@@ -16,6 +16,10 @@ module V1
     end
 
     private
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
 
     def invalid_login_attempt
       warden.custom_failure!
