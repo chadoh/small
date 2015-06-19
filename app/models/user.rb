defb986d61f3374d62e7529f57ac83cd69ef5481
Chad Ostrowski <hi@chadoh.com>
2015-06-19 13:51:03 -0400

    Add super-simple token authentication
    Things I'd want to do in this stage for a real app:
    
    * tests
    * some sort of secure api auth system

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :update_access_token!
+
+  validates :username, presence: true
+  validates :email, presence: true
+
+  private
+
+  def update_access_token!
+    update! access_token: "#{self.id}:#{Devise.friendly_token}"
+  end
 end
